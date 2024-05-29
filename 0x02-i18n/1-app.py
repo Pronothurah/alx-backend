3c22e870dd57c01933b8b69734db239fd196430a
Pronothurah <onsongojosphat28@gmail.com>
2024-05-29 20:16:56 +0300

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 
 class Config:
+    """has a LANGUAGES class attribute """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
