5226d6cca2bdfe56f3c2be47f5400cc412382b6e
Pronothurah <onsongojosphat28@gmail.com>
2024-06-26 20:53:21 +0300

    1-redis_op.js

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -11,3 +11,22 @@ client.on('connect', () => {
 client.on('error', (err) => {
     console.error(`Redis client not connected to the server: ${err.message}`);
 });
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (err, reply) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(reply);
+    });
+}
+
+// Calling the functions as specified
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
